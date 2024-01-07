a1cb44f10b71919b015fdae9e0f47ca4bfb1881a
Bao Hoc Hoi <baozzskt2k4@gamil.com>
2024-01-07 21:11:33 +0700

    init git

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+package tes1;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("hello World from java");
+		System.out.println("hello World from java");
+		System.out.println("hello World from java");
+		System.out.println("hello World from java");
+		System.out.println("hello World from java");
+	}
+}
